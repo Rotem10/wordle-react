6923fb6b817e7eeae141837bc558776bcc1e450f
Rotem10 <65648972+Rotem10@users.noreply.github.com>
2023-01-11 23:57:58 +0200

    delete folders

--- src/providers/appContext.tsx
+++
@@ -1,6 +0,0 @@
-import { createContext } from 'react';
-type SetTile = React.Dispatch<React.SetStateAction<number>>;
-type UseApp = { currentTile: number; setCurrentTile: SetTile };
-
-export const appContext: React.Context<null> | React.Context<UseApp> =
-  createContext(null);
